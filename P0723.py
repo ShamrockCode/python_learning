3d1ce7db7abad139bc1b7ab5bc53a89dc4b8c22f
ShamrockCode <1360498907@qq.com>
2023-07-23 23:02:26 +0100

    数据类型

---
+++ P0723.py
@@ -0,0 +1,25 @@
+# def reverseWords(input):
+#
+#     inputWords = input.split(" ")
+#     print(inputWords)
+#
+#     inputWords = inputWords[-1::-1]
+#     print(inputWords)
+#
+#     output = ' '.join(inputWords)
+#     print(output)
+#
+#     return output
+#
+#
+# if __name__ == "__main__":
+#     input = 'I like runoob'
+#     rw = reverseWords(input)
+#     print(rw)
+
+# 元组（Tuple）类似于列表，不同之处在于元组的元素不能修改。
+# 元组写在小括号（）里，元素之间用逗号隔开。
+# 元组中的元素类型也可以不相同
+a = 17
+a = hex(a)
+print(a)
